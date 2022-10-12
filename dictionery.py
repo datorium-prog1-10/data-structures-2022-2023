f78c750112f63dc0e6a1b17a6d4de5c829054646
Datorium <hello@datorium.eu>
2022-10-12 11:34:03 +0300

    Create dictionery.py

---
+++ dictionery.py
@@ -0,0 +1,29 @@
+contacts = [
+    {
+        'name': 'Anna', 
+        'age': 17,
+        'phone': '654987321'
+    }, 
+    {
+        'name': 'Oskars', 
+        'age': 22,
+        'phone': '+371 1234679654'
+    }, 
+    {
+        'name': 'Jenifer', 
+        'age': 18,
+        'phone': '+1(987)6547852147'
+    },
+    {
+        'name': 'Miks', 
+        'age': 16,
+        'phone': '4658784125'
+    }
+]
+
+for contact in contacts:
+    print(contact['name'])
+    print(contact['age'])
+    print(contact['phone'])
+
+
